424173ff6f520208138a60d55db400d3be8d2dc8
lsmucassi <lindasmucassi@gmail.com>
2020-07-17 20:32:51 +0200

    promodoro

---
+++ src/components/Promodoro.vue
@@ -0,0 +1,23 @@
+<template>
+    <v-card>
+        <v-tabs v-model="timerType" >
+            <v-tab
+                v-for="tab in tabsTitles"
+                :key="tab"
+                >
+                {{ tab }}
+            </tab>
+        </v-tabs>
+    </v-card>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            timerType: 0,
+            tabsTitles: ['Promodoro', 'Short Break', 'Long Break']
+        }
+    }
+}
+</script>
